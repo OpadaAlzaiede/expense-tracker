5f740b7da3e6e0b56303b238efbb2cb852b19ef6
obada <opadaalziede@gmail.com>
2024-08-05 12:27:44 +0300

    add validation & session middlewares

--- configs/middleware.php
+++ configs/middleware.php
@@ -3,6 +3,9 @@
 declare(strict_types = 1);
 
 use App\Config;
+use App\Middleware\OldFormDataMiddleware;
+use App\Middleware\StartSessionMiddleware;
+use App\Middleware\ValidationErrorsMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
 use Slim\Views\Twig;
@@ -16,7 +19,9 @@ return function (App $app) {
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
 
     $app->add(ValidationExceptionMiddleware::class);
-
+    $app->add(ValidationErrorsMiddleware::class);
+    $app->add(OldFormDataMiddleware::class);
+    $app->add(StartSessionMiddleware::class);
 
     // Logger
     $app->addErrorMiddleware(
